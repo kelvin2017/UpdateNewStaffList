b5592b80ae78d7ba890b86e5e903f6c124e815d5
Kelvin Wong <wong.wing@chunwo.com>
2017-09-15 15:43:03 +0800

    Update Staff List API

---
+++ UpdateNewStaffList/Class/AddSPItemList.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net;
+using CWAppsCore.Helpers;
+using System.Configuration;
+
+
+namespace UpdateNewStaffList.Class
+{
+    public class AddSPItemList
+    {
+        public void DeleteNewStafflist()
+        {
+            
+
+        }
+
+        public void AddNewStafflist(List<string> lstStaff)
+        {
+            try
+            {
+                //Get App.config
+                Configuration configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                AppSettingsSection appSettings = configuration.AppSettings;
+
+                com.chunwo.dms.Lists lstService = new com.chunwo.dms.Lists();
+                lstService.Credentials = CredentialCache.DefaultCredentials;
+                lstService.Url = appSettings.Settings["PhonebookService"].Value;
+
+                string strLogin = appSettings.Settings["PhonebookServiceID"].Value;
+                string strPw = appSettings.Settings["PhonebookServicePW"].Value;
+                string strDomain = appSettings.Settings["PhonebookServiceDomain"].Value;
+
+                //lstService.Url = "http://dms.chunwo.com/sites/teamsupport/phonebook/_vti_bin/Lists.asmx";
+
+                //lstService.Credentials = new NetworkCredential("spadmin", "P@ssw0rd", "CHUNWO");
+                //lstService.Credentials = new NetworkCredential("wong.wing", "97476228@Mm", "CHUNWO");
+                lstService.Credentials = new NetworkCredential(strLogin, strPw, strDomain);
+
+
+                System.Xml.XmlDocument doc = new System.Xml.XmlDocument();
+                System.Xml.XmlElement batchElement = doc.CreateElement("Batch");
+
+                string strBatch = "<Method ID='1' Cmd='New'>" +
+                    "<Field Name ='Title'>" + lstStaff[0] + "</Field>" +
+                    "<Field Name ='Staff_x0020_Number'>" + lstStaff[1] + "</Field>" +
+                    "<Field Name ='Full_x0020_Name'>" + lstStaff[2] + "</Field>" +
+                    "<Field Name ='First_x0020_Name'>" + lstStaff[3] + "</Field>" +
+                    "<Field Name ='Last_x0020_Name'>" + lstStaff[4] + "</Field>" +
+                    "<Field Name ='Nick_x0020_Name'>" + lstStaff[5] + "</Field>" +
+                    "<Field Name ='Department'>" + lstStaff[6] + "</Field>" +
+                    "<Field Name ='DepartmentDesc'>" + lstStaff[7].StringToURLEncode() + "</Field>" +
+                    "<Field Name ='Company_x0020_Co'>" + lstStaff[8] + "</Field>" +
+                    "<Field Name ='PositionCode'>" + lstStaff[9] + "</Field>" +
+                    "<Field Name ='PositionDesc'>" + lstStaff[10].StringToURLEncode() + "</Field>" +
+                    "<Field Name ='Email'>" + lstStaff[11] + "</Field>" +
+                    "<Field Name ='ADEnable'>" + lstStaff[12] + "</Field>" +
+                    "<Field Name ='Division'>" + lstStaff[13] + "</Field>" +
+                    "<Field Name ='ChineseName'>" + lstStaff[14] + "</Field>" +
+                    "</Method>";
+
+                batchElement.SetAttribute("OnError", "Continue");
+                batchElement.SetAttribute("ListVersion", "1");
+                batchElement.InnerXml = strBatch.ToString();
+
+                //Add To Server
+                lstService.UpdateListItems("New Staff List", batchElement);
+                lstService.Dispose();
+
+            }
+            catch (Exception ex)
+            {
+                SystemLog.filePath = Directory.GetCurrentDirectory() + "\\sys.log";
+                SystemLog.log(ex.ToString());
+            }
+
+        }
+
+    }
+}
---
+++ UpdateNewStaffList/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace UpdateNewStaffList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblLoad = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblLoad
+            // 
+            this.lblLoad.AutoSize = true;
+            this.lblLoad.Font = new System.Drawing.Font("細明體", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblLoad.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblLoad.Location = new System.Drawing.Point(15, 9);
+            this.lblLoad.Name = "lblLoad";
+            this.lblLoad.Size = new System.Drawing.Size(457, 32);
+            this.lblLoad.TabIndex = 0;
+            this.lblLoad.Text = "Uploading...Please Waiting";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 61);
+            this.Controls.Add(this.lblLoad);
+            this.Name = "Form1";
+            this.Text = "Update New Staff List";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLoad;
+    }
+}
+
